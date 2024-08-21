efbbdc1be4f261bffda8b445e3e8ed52789045c1
izancode <sf48116@gmail.com>
2024-08-21 12:29:13 +0530

    working

--- models/studentModel.js
+++ models/studentModel.js
@@ -4,75 +4,75 @@ const studentSchema = new Schema(
   {
     student_first_name: {
       type: String,
-      // required: [true, "Please enter your name"],
+      required: [true, "Please enter your name"],
     },
     student_middle_name: {
       type: String,
-      // required: [true, "Please enter your middle name"],
+      required: [true, "Please enter your middle name"],
     },
     student_last_name: {
       type: String,
-      // required: [true, "Please enter your last name"],
+      required: [true, "Please enter your last name"],
     },
     student_nationality: {
       type: String,
-      // required: [true, "Please enter your nationality"],
+      required: [true, "Please enter your nationality"],
     },
     student_address: {
       type: String,
-      // required: [true, "Please enter your address"],
+      required: [true, "Please enter your address"],
     },
     student_apartment: {
       type: String,
-      // required: [true, "Please enter your apartment"],
+      required: [true, "Please enter your apartment"],
     },
     student_country: {
       type: String,
-      // required: [true, "Please enter your country"],
+      required: [true, "Please enter your country"],
     },
     student_state: {
       type: String,
-      // required: [true, "Please enter your state"],
+      required: [true, "Please enter your state"],
     },
     student_city: {
       type: String,
-      // required: [true, "Please enter your city"],
+      required: [true, "Please enter your city"],
     },
     student_postal_code: {
       type: Number,
-      // required: [true, "Please enter your postal code"],
+      required: [true, "Please enter your postal code"],
     },
     student_phone_number: {
       type: Number,
-      // required: [true, "Please enter your phone number"],
+      required: [true, "Please enter your phone number"],
     },
     student_email: {
       type: String,
-      // required: [true, "Please enter your email"],
+      required: [true, "Please enter your email"],
     },
     dob: {
       type: String,
-      // required: [true, "Please enter your Date of Birth"],
+      required: [true, "Please enter your Date of Birth"],
     },
     student_gender: {
       type: String,
-      // required: [true, "Please enter your gender"],
+      required: [true, "Please enter your gender"],
     },
     student_blood_group: {
       type: String,
-      // required: [true, "Please enter your blood group"],
+      required: [true, "Please enter your blood group"],
     },
     student_caste_category: {
       type: String,
-      // required: [true, "Please enter your caste category"],
+      required: [true, "Please enter your caste category"],
     },
     student_instagram_url: {
       type: String,
-      // required: [true, "Please enter your instagram url"],
+      required: [true, "Please enter your instagram url"],
     },
     student_linkedin_url: {
       type: String,
-      // required: [true, "Please enter your linkedin url"],
+      required: [true, "Please enter your linkedin url"],
     },
   },
   {
