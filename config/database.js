706a15e83924d1489c8fa1fc06a7a016b3b0faac
izancode <sf48116@gmail.com>
2024-08-21 12:12:18 +0530

    mongodb uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDatabase = () => {
+  console.log("MongoDB URI:", process.env.DB_URI); // Debug line to check the URI
+
   mongoose
     .connect(process.env.DB_URI, { serverSelectionTimeoutMS: 50000 })
     .then((data) =>
