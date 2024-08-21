a167eaf23436f31c3c4b76e7440fc260dadd29a8
izancode <sf48116@gmail.com>
2024-08-21 11:15:50 +0530

    changes 2

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => res.send("Express faizan on Vercel"));
+app.get("/", (req, res) =>
+  res.send("Express faizan  on Vercel la la la vicenbzo")
+);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
