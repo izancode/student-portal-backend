4545c75348f3678070aa1af7792f025d30fda921
izancode <sf48116@gmail.com>
2024-08-21 13:14:56 +0530

    home chaanges

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import studentRoute from "./routes/studentRoute.js";
+import homeRoute from "./routes/homeRouts.js";
 const app = express();
 app.use(express.json());
 
-// app.use("/", studentRoute);
+app.use("/", homeRoute);
 app.use("/api/v1", studentRoute);
 
 export default app;
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,3 @@
-import studentModel from "../models/studentModel.js";
-
 export const home = async (req, res) => {
   res.send("<h1>Working</h1>");
 };
