e39d2fb592252f372ac4104f8d9846a5898ca763
izancode <sf48116@gmail.com>
2024-08-26 14:33:30 +0530

    cors add 2

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ import cors from "cors";
 import studentRoute from "./routes/studentRoute.js";
 import homeRoute from "./routes/homeRouts.js";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Allows all origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 app.use(express.json());
 
 app.use("/", homeRoute);
