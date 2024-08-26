47d81e5f8a333c93004ad3c3b1c1fadd6a5a8981
izancode <sf48116@gmail.com>
2024-08-26 13:32:52 +0530

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
-
+import cors from "cors";
 import studentRoute from "./routes/studentRoute.js";
 import homeRoute from "./routes/homeRouts.js";
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/", homeRoute);
