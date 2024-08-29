40b4495f387988e290121c23c88a38b1e1ec2977
izancode <sf48116@gmail.com>
2024-08-29 13:32:50 +0530

    student Controllers error updated 2

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -5,6 +5,6 @@ export const signInStudent = async (req, res) => {
     const student = await studentModel.create(req.body);
     res.status(200).json(student);
   } catch (error) {
-    res.status(500).json({ message: error });
+    res.status(500).json({ message: error.message });
   }
 };
