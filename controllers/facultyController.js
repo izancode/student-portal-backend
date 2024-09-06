1a31a5e050cb9975bd50b8c09eba972414059a43
izancode <sf48116@gmail.com>
2024-09-06 22:25:18 +0530

    solving some duplicate image are uploaded on cloudinary issue has been solved now

--- controllers/facultyController.js
+++ controllers/facultyController.js
@@ -6,7 +6,7 @@ export const signInFaculty = async (req, res) => {
 
     const facultyData = { ...req.body, faculty_profile_image: fileUrl };
     const faculty = await facultyModel.create(facultyData);
-    console.log(faculty);
+  
     res.status(200).json(faculty);
   } catch (error) {
     res.status(500).json({ message: error.errorResponse });
