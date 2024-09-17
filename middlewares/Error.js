f4e42e8b339889967b566756fec50159b550a691
izancode <sf48116@gmail.com>
2024-09-17 14:08:51 +0530

    writin better error to show from frontend side

--- middlewares/Error.js
+++ middlewares/Error.js
@@ -1,9 +1,11 @@
 const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
-  console.log(err);
+  console.log("coning from error.js : ", err);
   if (err.code === 11000) {
-    err.message = "Duplicate Key Error";
+    let key = Object.keys(err.keyValue)[0];
+    key = key.replace(/_/g, " ");
+    err.message = `The ${key} already exists`;
     err.statusCode = 400;
   }
   res.status(err.statusCode).json({
