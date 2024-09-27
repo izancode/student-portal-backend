bf4bebea76d3fc2724237c9c167c06fe95c3409f
izancode <izancode11@gmail.com>
2024-09-27 19:37:44 +0530

    otp on mail is sending properly issue solved

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -59,7 +59,7 @@ export const logInStudent = async (req, res, next) => {
     await senderService(user, otp, otpExpiry);
     res.status(200).json({
       status: true,
-      message: `OTP has been sent to your emai-id ${user.student_email + otp}`,
+      message: `OTP has been sent to your emai-id ${user.student_email}`,
       identifier: user.student_email,
     });
   } catch (error) {
