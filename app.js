5d2f892c22733a0cc93e08bb460b4c5c0ae5850b
izancode <sf48116@gmail.com>
2024-11-10 21:17:59 +0530

    test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false,
+    credentials: true,
   })
 );
 app.use(express.json());
