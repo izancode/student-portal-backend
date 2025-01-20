446a4d63c0926cdcf2b696a70055cfd2ff8a1ccb
izancode <sf48116@gmail.com>
2025-01-20 18:07:25 +0530

    cookies testing 1

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-  import express from "express";
-  import cors from "cors";
-  import cookieParser from "cookie-parser";
-  import errorMiddleware from "./middlewares/Error.js";
-  import homeRoute from "./routes/homeRouts.js";
-  import studentRoute from "./routes/studentRoute.js";
-  import facultyRoute from "./routes/facultyRoute.js";
-  import userLoginRoute from "./routes/userLoginRoute.js";
-  import userSingleRoute from "./routes/userSingleRoute.js";
-  import userUpdateRoute from "./routes/userUpdateRoute.js";
-  const app = express();
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://student-portal-frontend-phi.vercel.app",
-  ];
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"), false);
-        }
-      },
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true,
-    })
-  );
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use("/", homeRoute);
-  app.use("/api/v1", studentRoute);
-  app.use("/api/v1", facultyRoute);
-  app.use("/api/v1", userLoginRoute);
-  app.use("/api/v1", userSingleRoute);
-  app.use("/api/v1", userUpdateRoute);
-  app.use(errorMiddleware);
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/Error.js";
+import homeRoute from "./routes/homeRouts.js";
+import studentRoute from "./routes/studentRoute.js";
+import facultyRoute from "./routes/facultyRoute.js";
+import userLoginRoute from "./routes/userLoginRoute.js";
+import userSingleRoute from "./routes/userSingleRoute.js";
+import userUpdateRoute from "./routes/userUpdateRoute.js";
+const app = express();
+app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://student-portal-frontend-phi.vercel.app",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"), true);
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/", homeRoute);
+app.use("/api/v1", studentRoute);
+app.use("/api/v1", facultyRoute);
+app.use("/api/v1", userLoginRoute);
+app.use("/api/v1", userSingleRoute);
+app.use("/api/v1", userUpdateRoute);
+app.use(errorMiddleware);
 
-  export default app;
+export default app;
