5d148801b0e22a7d980bdf864911f9f9bd2d706b
izancode <sf48116@gmail.com>
2025-01-21 10:20:47 +0530

    testing cookies

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "./middlewares/Error.js";
-import allowCors from "./middlewares/cors.js";
 import homeRoute from "./routes/homeRouts.js";
 import studentRoute from "./routes/studentRoute.js";
 import facultyRoute from "./routes/facultyRoute.js";
@@ -9,10 +9,26 @@ import userLoginRoute from "./routes/userLoginRoute.js";
 import userSingleRoute from "./routes/userSingleRoute.js";
 import userUpdateRoute from "./routes/userUpdateRoute.js";
 const app = express();
-
-app.use(cookieParser());
-app.use(allowCors);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://student-portal-frontend-phi.vercel.app",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"), false);
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 app.use("/", homeRoute);
 app.use("/api/v1", studentRoute);
 app.use("/api/v1", facultyRoute);
